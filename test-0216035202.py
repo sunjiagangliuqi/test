0dce1f506d4f93d0e0ddf8c39ec1f06c817c22cd
sunjiagangliuqi <59460398+sunjiagangliuqi@users.noreply.github.com>
2023-02-16 11:52:03 +0800

    Pushed DAG test-0216035202

---
+++ test-0216035202.py
@@ -0,0 +1,25 @@
+from airflow.operators.bash_operator import BashOperator
+from airflow import DAG
+from airflow.utils.dates import days_ago
+
+args = {
+    "project_id": "test-0216035202",
+}
+
+dag = DAG(
+    "test-0216035202",
+    default_args=args,
+    schedule_interval="@once",
+    start_date=days_ago(1),
+    description="""
+Created with Elyra 3.14.2 pipeline editor using `test.pipeline`.
+    """,
+    is_paused_upon_creation=False,
+)
+
+
+# Operator source: {"catalog_type": "local-file-catalog", "component_ref": {"base_dir": "/home/jovyan/work/test/component", "path": "add.py"}}
+
+op_0ad6234a_b1a1_461e_8883_2f084ea38948 = BashOperator(
+    task_id="BashOperator", a="12", b="15", executor_config={}, dag=dag
+)
